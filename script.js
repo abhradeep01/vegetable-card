6ba3dfeba04a1217e2b81badec5e1bb1e3bc7e85
lutiousXt54 <abhradeepdebnath@gmail.com>
2024-03-14 20:03:36 +0530

    adding js file and some features to the web

---
+++ script.js
@@ -0,0 +1,12 @@
+function socialLink(){
+    const social = document.querySelector('.sub_contact');
+    const links =['https://www.facebook.com/abhradeep.debnath','https://twitter.com/AbhradeepD74811','https://www.instagram.com/earlysons/','https://www.youtube.com/'];
+    for(let i=0;i<social.children.length;i++){
+        social.children[i].addEventListener('click',(e)=>{
+            // window.location.href=links[i];
+            // e.preventDefault();
+            window.open(links[i], '_blank');
+        })
+    }
+}
+socialLink();
