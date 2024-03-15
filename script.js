8f76662b80a597f151cbce8cc35732920794379a
lutiousXt54 <abhradeepdebnath@gmail.com>
2024-03-15 13:25:02 +0530

    adding js file and some feature

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ function socialLink(){
     const links =['https://www.facebook.com/abhradeep.debnath','https://twitter.com/AbhradeepD74811','https://www.instagram.com/earlysons/','https://www.youtube.com/'];
     for(let i=0;i<social.children.length;i++){
         social.children[i].addEventListener('click',(e)=>{
-            // window.location.href=links[i];
-            // e.preventDefault();
             window.open(links[i], '_blank');
         })
     }
